0d5145797aa899d45a7059930654c0022c4c4feb
QuyTuanVu <quytuan.vu@bbsw.io>
2022-07-19 11:24:09 +0700

    Header CSS: logo + search input

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,35 @@
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
+import images from '~/assets/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 
 
 function Header() {
 
   const cx = classNames.bind(styles)
-  
+
     return (
       <header className={cx('wrapper')}>
         <div className={cx('inner')}>
+          <div className={cx('logo')}>
+            <img alt='TikTok' src={images.logo}/>
+          </div>
+          <div className={cx('search')}>
+            <input placeholder='Search accounts and videos' spellCheck={false} />
+            <button className={cx('clear')}>
+              <FontAwesomeIcon icon={faCircleXmark}/>
+            </button>
+            <FontAwesomeIcon icon={faSpinner} className={cx('loading')}/>
+            <button className={cx('search-btn')}> 
+              <FontAwesomeIcon icon={faMagnifyingGlass}/>
+            </button>
+          </div>
+
+          <div className={cx('action')}>
+            
+          </div>
         </div>
       </header>
     )
